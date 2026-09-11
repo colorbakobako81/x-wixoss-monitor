4e7a2ec73de6a42b8615d249f1c76574bf3088f5
colorbakobako81 <kfield.cure81@gmail.com>
2026-09-11 15:59:46 +0900

    Update fxtwitter_monitor.py

--- fxtwitter_monitor.py
+++ fxtwitter_monitor.py
@@ -2,7 +2,7 @@ import os
 import requests
 from pathlib import Path
 
-USERNAME = "wixoss_TCG"
+USERNAME = "mirudake3634"
 API_URL = f"https://api.fxtwitter.com/2/profile/{USERNAME}/statuses?count=20"
 
 DISCORD_WEBHOOK_URL = os.environ["DISCORD_WEBHOOK_URL"]
